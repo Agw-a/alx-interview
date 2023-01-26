31a4bc8f624bd0e2b6ddfe57a3614b93fd866d93
Agw-a <lizagwa7@gmail.com>
2023-01-26 19:29:33 +0300

    Remove type notation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 '''UTF-8 Validation
 '''
-from typing import List
 
 
-def validUTF8(data: List[int]) -> bool:
+def validUTF8(data):
     """determine if a dataset represent valid UTF=8
 
     Args:
