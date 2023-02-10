2a810cb349d2612f37272e27721e2152a1f128fc
Agw-a <lizagwa7@gmail.com>
2023-02-10 09:43:32 +0300

    n-queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import sys
+
+
+# validate input
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
+
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+
+board_size = int(sys.argv[1])
+
+
+def getNqueens(n, i=0, a=[], b=[], c=[]):
+    """ validate queen positions on the board """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from getNqueens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+
+def solve(n):
+    """ print placements on the board """
+    k = []
+    i = 0
+    for solution in getNqueens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(board_size)
