5d70d929d0cab956c69500dc272dbfde522633a1
Agw-a <lizagwa7@gmail.com>
2023-02-11 12:47:43 +0300

    change input check statements

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -28,18 +28,17 @@ def Fill_Rows(board, row):
 
 
 # validate input
-n = int(sys.argv[1])
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
-    exit(1)
-
-if not sys.argv[1].isdigit():
+    sys.exit(1)
+try:
+    n = int(sys.argv[1])
+except:
     print("N must be a number")
-    exit(1)
-
-if int(sys.argv[1]) < 4:
+    sys.exit(1)
+if n < 4:
     print("N must be at least 4")
-    exit(1)
+    sys.exit(1)
 
 board = [-1 for i in range(n)]
 Fill_Rows(board, 0)
