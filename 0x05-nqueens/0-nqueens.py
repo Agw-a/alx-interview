ef2b8779889dace51953e96fbc36d16e690da300
Agw-a <lizagwa7@gmail.com>
2023-02-11 12:37:23 +0300

    n-queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,7 +4,31 @@ import sys
 '''
 
 
+def isSafe(board, row, i):
+    '''checks if it's okay to insert a queen at colunm i in a row on the board
+    '''
+    for q in range(row):
+        if (board[q] == i or
+                board[q] + row - q == i or
+                board[q] + q - row == i):
+            return False
+    return True
+
+
+def Fill_Rows(board, row):
+    '''Fills rows in board with right index
+    '''
+    for q in range(len(board)):
+        if isSafe(board, row, q):
+            board[row] = q
+            if row < len(board) - 1:
+                Fill_Rows(board, row + 1)
+            else:
+                print([[q, board[q]] for i in range(len(board))])
+
+
 # validate input
+n = int(sys.argv[1])
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
@@ -17,4 +41,5 @@ if int(sys.argv[1]) < 4:
     print("N must be at least 4")
     exit(1)
 
-board_size = int(sys.argv[1])
+board = [-1 for i in range(n)]
+Fill_Rows(board, 0)
