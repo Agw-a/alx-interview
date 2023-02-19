623bda6355ac852f4424df9753491e3e81a45b8b
Agw-a <lizagwa7@gmail.com>
2023-02-19 11:45:58 +0300

    Rotate matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,29 +4,18 @@
 
 
 def rotate_2d_matrix(matrix):
-    """rotate a 2D matrix clockwise
+    """rotates a 2D matrix clockwise
 
     Args:
         2D matrix
     """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    for i in range(len(matrix)):
+        for n in range(i):
+            temp = matrix[i][n]
+            matrix[i][n] = matrix[n][i]
+            matrix[n][i] = temp
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix) // 2):
+            temp = matrix[i][j]
+            matrix[i][len(matrix) - j - 1] = temp
